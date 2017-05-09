eaed8af6f09f4ce681d39af45be76ffc6a5ace3a
Illia Ratkevych <illia.ratkevych@gmail.com>
2017-05-09 22:26:12 +0300

    Fix test conditions and output.

--- src/Pather/Program.cs
+++ src/Pather/Program.cs
@@ -11,21 +11,24 @@ namespace Pather
             ////var fileContent = File.ReadAllText(filePath);
 
             var testText1 =
-                "....................." +
-                "...#................." +
-                "....................." +
-                "....................." +
-                "....................." +
-                "...#................." +
+                "....................." + Environment.NewLine +
+                "...#................." + Environment.NewLine +
+                "....................." + Environment.NewLine +
+                "....................." + Environment.NewLine +
+                "....................." + Environment.NewLine +
+                "...#................." + Environment.NewLine +
                 ".....................";
 
             var map = new MapLoader().Parse(testText1);
-            var resultMap = new PathFinder().FindPathes(map);
-
-            Console.WriteLine("Result map:");
+            Console.WriteLine("Input:");
+            Console.WriteLine(map.ToString());
             Console.WriteLine();
+
+            var resultMap = new PathFinder().FindPathes(map);
+            Console.WriteLine("Output:");
             Console.WriteLine(resultMap.ToString());
             Console.WriteLine();
+
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
         }
